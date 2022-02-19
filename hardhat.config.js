c33996a52b695405261a3758dfda3160611a8088
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-02-19 17:59:49 -0500

    supports ether and rinkeby deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const { PRIVATE_KEY, POLYGON_URL, RINKEBY_URL } = process.env;
 
 module.exports = {
   solidity: "0.7.5",
   paths: {
     artifacts: "./app/artifacts",
-  }
+  },
+  networks: {
+    rinkeby: {
+      url: RINKEBY_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    mumbai: {
+      url: POLYGON_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
